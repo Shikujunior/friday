221ad8c5cbde64adf777720edb12910f5a6fce37
tezz <hadassahtezz@gmail.com>
2023-06-29 15:04:22 +0300

    commit 2
    addition of js numbers

---
+++ index.js
@@ -0,0 +1,15 @@
+let num1=1;
+let num2=2;
+let num3=3;
+let num4=4;
+let num5=5;
+let num6=6;
+let num7=7;
+let num8=8;
+let num9=9;
+let num10=10;
+let num11=11;
+let num12=12;
+let num13=13;
+let num14=14;
+let num15=15;
